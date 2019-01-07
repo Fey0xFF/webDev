196aa7606b0af149eb5d8e8c264c78519a0ebbac
Anthony <anthony.bendas@gmail.com>
2019-01-06 20:28:34 -0800

    function declaration, expression

--- js-basics/script.js
+++ js-basics/script.js
@@ -1,7 +1,29 @@
-4 + 3;
+// 4 + 3;
+// if (4+3 === 7) {
+// 	alert("You're smart!");
+// }
 
-if (4+3 === 7) {
-	alert("You're smart!");
+function sayHello() {
+	console.log("Hello");
 }
 
-console.log("Helloooo");
+sayHello();
+
+var sayBye = function() {
+	console.log("Bye");
+};
+
+sayBye();
+
+function sing(song) {
+	console.log(song);
+}
+
+sing("ahhhhh");
+
+function multiply(a, b) {
+	return a * b;
+}
+
+multiply(5,10);
+alert(multiply(10,1000));
