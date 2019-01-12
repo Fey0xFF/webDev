d5fb667dc6b3e1b7f5a13ef1227617722ae8dd0e
Anthony Bendas <anthony.bendas@gmail.com>
2019-01-12 07:59:21 -0800

    keypress function and refactoring

--- dom/script.js
+++ dom/script.js
@@ -3,15 +3,41 @@ var input = document.getElementById("userinput");
 var list = document.querySelector("ul");
 var removeButton = document.getElementById("remove");
 
+function addItem() {
+		var li = document.createElement("li");
+		if (input.value.length < 1) {
+			alert("Nothing entered!");
+		} else {
+			li.appendChild(document.createTextNode(input.value));
+			list.appendChild(li);	
+		}
+		input.value = "";
+};
+
 button.addEventListener("click", function() {
-	var li = document.createElement("li");
-	if (input.value.length < 1) {
-		alert("Nothing entered!");
-	} else {
-		li.appendChild(document.createTextNode(input.value));
-		list.appendChild(li);	
-	}
-	input.value = "";
+	// var li = document.createElement("li");
+	// if (input.value.length < 1) {
+	// 	alert("Nothing entered!");
+	// } else {
+	// 	li.appendChild(document.createTextNode(input.value));
+	// 	list.appendChild(li);	
+	// }
+	// input.value = "";
+	addItem();
+});
+
+input.addEventListener("keypress", function(e) {
+	if (e.keyCode === 13) {
+		// var li = document.createElement("li");
+		// if (input.value.length < 1) {
+		// 	alert("Nothing entered!");
+		// } else {
+		// 	li.appendChild(document.createTextNode(input.value));
+		// 	list.appendChild(li);	
+		// }
+		// input.value = "";
+		addItem();		
+	};
 });
 
 remove.addEventListener("click", function() {
