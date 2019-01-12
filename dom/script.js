86e4bf871afb6abea2741c7e62baa47148b51a24
Anthony Bendas <anthony.bendas@gmail.com>
2019-01-12 08:19:45 -0800

    refactored check input, add items, and event listeners

--- dom/script.js
+++ dom/script.js
@@ -3,44 +3,36 @@ var input = document.getElementById("userinput");
 var list = document.querySelector("ul");
 var removeButton = document.getElementById("remove");
 
+function checkInputToAdd() {
+	if (input.value.length > 0) {
+		return true;
+	} else {
+		return alert("Nothing entered!");
+	}
+}
+
 function addItem() {
+	if (checkInputToAdd()) {
 		var li = document.createElement("li");
-		if (input.value.length < 1) {
-			alert("Nothing entered!");
-		} else {
-			li.appendChild(document.createTextNode(input.value));
-			list.appendChild(li);	
-		}
+		li.appendChild(document.createTextNode(input.value));
+		list.appendChild(li);	
 		input.value = "";
-};
+	}
+}
 
-button.addEventListener("click", function() {
-	// var li = document.createElement("li");
-	// if (input.value.length < 1) {
-	// 	alert("Nothing entered!");
-	// } else {
-	// 	li.appendChild(document.createTextNode(input.value));
-	// 	list.appendChild(li);	
-	// }
-	// input.value = "";
-	addItem();
-});
-
-input.addEventListener("keypress", function(e) {
+function addItemAfterPress(e) {
 	if (e.keyCode === 13) {
-		// var li = document.createElement("li");
-		// if (input.value.length < 1) {
-		// 	alert("Nothing entered!");
-		// } else {
-		// 	li.appendChild(document.createTextNode(input.value));
-		// 	list.appendChild(li);	
-		// }
-		// input.value = "";
-		addItem();		
-	};
-});
+		if (checkInputToAdd()) {
+			addItem();	
+		}
+	}
+}
 
-remove.addEventListener("click", function() {
+function removeLastItem() {
 	var itemToRemove = input.value;
 	list.removeChild(list.lastChild);
-});
+}
+
+button.addEventListener("click", addItem);
+input.addEventListener("keypress", addItemAfterPress);
+remove.addEventListener("click", removeLastItem);
