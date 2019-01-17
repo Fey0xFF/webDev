e34aaf70077c354f36280dff536f765bbee5b047
Anthony <anthony.bendas@gmail.com>
2019-01-16 21:22:44 -0800

    display linear-gradient for copy pasting

--- background-generator/script.js
+++ background-generator/script.js
@@ -9,7 +9,8 @@ function updateColors() {
 		+ color1.value 
 		+ ", " 
 		+ color2.value 
-		+ ")";	
+		+ ")";
+		css.textContent = body.style.background + ";";	
 }
 
 color1.addEventListener("input", updateColors);
