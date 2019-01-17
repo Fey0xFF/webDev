e123e68ec622aa247d0f4093f9109acdb3a4d4f7
Anthony <anthony.bendas@gmail.com>
2019-01-16 21:16:47 -0800

    refactored color updating to a function

--- background-generator/script.js
+++ background-generator/script.js
@@ -3,21 +3,15 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-color1.addEventListener("input", function() {
+function updateColors() {
 	body.style.background = 
 		"linear-gradient(to right, " 
 		+ color1.value 
 		+ ", " 
 		+ color2.value 
-		+ ")";
-})
+		+ ")";	
+}
 
-color2.addEventListener("input", function() {
-	body.style.background = 
-		"linear-gradient(to right, " 
-		+ color1.value 
-		+ ", " 
-		+ color2.value 
-		+ ")";
-})
+color1.addEventListener("input", updateColors);
+color2.addEventListener("input", updateColors);
 
