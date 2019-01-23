fe1e98ff4c0289794ab4c297ddff6fd00ff8af32
Anthony <anthony.bendas@gmail.com>
2019-01-22 19:50:44 -0800

    iterating and enumerating over arrays and objects respectively

--- scope/advanced-looping-exercise.js
+++ scope/advanced-looping-exercise.js
@@ -33,26 +33,48 @@ const array2 = ['a', 3, 4, 2] // should return 4
 const array3 = [] // should return 0
 
 function biggestNumberInArray(arr) {
-
+  let greatest = 0;
+  arr.forEach(item => {
+    if (item > greatest) {
+      greatest = item;
+    }
+  })
+  return greatest;
 }
 
 function biggestNumberInArray2(arr) {
-
+  let greatest = 0;
+  for (item of arr) {
+    if (item > greatest) {
+      greatest = item;
+    }
+  }
+  return greatest;
 }
 
 function biggestNumberInArray3(arr) {
-
+  let greatest = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > greatest) {
+      greatest = arr[i];
+    }
+  }
+  return greatest;
 }
 
 
 // Question #2:
 // Write a function checkBasket() that lets you know if the item is in the basket or not
 amazonBasket = {
-  glasses: 1
-  books: 2
+  glasses: 1,
+  books: 2,
   floss: 100
 }
 
 function checkBasket(basket, lookingFor) {
-
+  if (basket.hasOwnProperty(lookingFor)) {
+    return `${lookingFor} is in the basket!`;
+  } else {
+    return `Sorry! ${lookingFor} not found!`;
+  }
 }
