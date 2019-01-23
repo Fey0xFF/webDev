e5907b0d9a4e0fe58d93d1da8a0a600a8f5dca75
Anthony <anthony.bendas@gmail.com>
2019-01-22 19:05:47 -0800

    solutions to exercise 8 with es8

--- scope/exerciseES8.js
+++ scope/exerciseES8.js
@@ -10,15 +10,15 @@ let rabbit = '🐇';
 '       🐢'
 '       🐇'
 
-// when you do:
 console.log(startLine);
-console.log(turtle);
-console.log(rabbit);
+console.log(turtle.padStart(9));
+console.log(rabbit.padStart(9));
 
 
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
+"🐢=======" 
 
 
 // #3) Get the below object to go from:
@@ -27,5 +27,14 @@ let obj = {
   is: 'Rudolf',
   the: 'raindeer'
 }
+
 // to this:
 'my name is Rudolf the raindeer'
+
+let newString = "";
+Object.entries(obj).map(item => {
+    item.forEach(word => {
+        newString += `${word} `;
+    })
+})
+newString = newString.trimEnd(1);
