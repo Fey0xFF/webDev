739fba0dbb5bed3c7928a3e18db960a9830d042e
Anthony Bendas <anthony.bendas@gmail.com>
2019-01-31 21:36:29 -0800

    promise.all on swapi api

--- api/promise/exercisePromises.js
+++ api/promise/exercisePromises.js
@@ -17,7 +17,7 @@ promise.then(value => console.log(value));
 // the above promise shorter with Promise.resolve() and console loggin "success"
 
 const promise = Promise.resolve(
-	
+
 );
 promise.then(value => console.log(value));
 
@@ -33,5 +33,14 @@ const urls = [
   'https://swapi.co/api/people/4'
 ]
 
+
+Promise.all(urls.map(url => {
+	return fetch(url).then(resp => resp.json())
+})).then(results => {
+	console.log(results[0])
+	console.log(results[1])
+	console.log(results[2])
+	console.log(results[3])
+}).catch(() => console.log('errrrorrr'))
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
