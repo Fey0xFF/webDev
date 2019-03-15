b00901544d3ba5926c68278ce51763981906d61d
Anthony Bendas <anthony.bendas@gmail.com>
2019-03-15 07:13:26 -0700

    node testing

---
+++ node/script.js
@@ -0,0 +1,11 @@
+//import largeNumber from 'script2.js';
+
+const largeNumber = require('./script2.js');
+
+const a = largeNumber.largeNumber;
+const b = 5;
+
+console.log(a + b);
+
+console.log(__dirname);
+
