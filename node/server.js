0a827427238714549c372095a8431843956cd31b
Anthony Bendas <anthony.bendas@gmail.com>
2019-03-17 17:24:53 -0700

    server now running with express

--- node/server.js
+++ node/server.js
@@ -1,17 +1,41 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-	console.log('headers',request.headers);
-	console.log('method',request.method);
-	console.log('url',request.url);
+const app = express();
 
-	const user = {
-		name: 'John',
-		hobby: 'skating'
-	}
-
-	response.setHeader('Content-Type', 'application/json');
-	response.end(JSON.stringify(user));
+app.get('/', (req, res) => {
+	res.send("Getting Root...");
 })
 
-server.listen(3000);
+app.get('/profile', (req, res) => {
+	res.send("Getting Profile...");
+})
+
+app.post('/profile', (req, res) => {
+	const user = {
+		name: "Sally",
+		hobby: "Soccer"
+	}
+	res.send(user);
+})
+
+app.listen(3000);
+
+
+// OLD CODE TESTING BASIC HTTP
+// const http = require('http');
+
+// const server = http.createServer((request, response) => {
+// 	console.log('headers',request.headers);
+// 	console.log('method',request.method);
+// 	console.log('url',request.url);
+
+// 	const user = {
+// 		name: 'John',
+// 		hobby: 'skating'
+// 	}
+
+// 	response.setHeader('Content-Type', 'application/json');
+// 	response.end(JSON.stringify(user));
+// })
+
+// server.listen(3000);
