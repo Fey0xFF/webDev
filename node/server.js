2ac801e3efda52d670ac208745a06d2100d60152
Anthony Bendas <anthony.bendas@gmail.com>
2019-03-17 17:32:51 -0700

    setting up app.use

--- node/server.js
+++ node/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log('<h1>HELLLOOO</h1>')
+	next();
+})
+
 app.get('/', (req, res) => {
 	res.send("Getting Root...");
 })
